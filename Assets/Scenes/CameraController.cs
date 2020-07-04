2da40403fdc95386e3717ad2a3cb284ea312b595
Ichiro Suzuki <suzukiichiro05@gmail.com>
2020-07-03 19:33:11 -0700

    First Commit Test

---
+++ Assets/Scenes/CameraController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CameraController : MonoBehaviour
+{
+
+    public float cameraSens = 200f;
+    public Transform playerBody;
+    float xRotation = 0f;
+    float yRotation = 0f;
+    float zRotation = 0f;
+    float rotationSpeed = 0.7f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float MouseXAxis = Input.GetAxis("Mouse X") * cameraSens * Time.deltaTime;
+        float MouseYAxis = Input.GetAxis("Mouse Y") * cameraSens * Time.deltaTime;
+        xRotation -= MouseYAxis;
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * MouseXAxis);
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        zRotation = Mathf.Clamp(zRotation, -5f, 5f);
+
+        if (Input.GetKey(KeyCode.D)) {
+            transform.localRotation = Quaternion.Euler(xRotation, yRotation, zRotation -= rotationSpeed);
+
+        }
+        else
+        {
+            transform.localRotation = Quaternion.Euler(xRotation, yRotation, zRotation);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.localRotation = Quaternion.Euler(xRotation, yRotation, zRotation += rotationSpeed);
+        }
+        
+
+
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            transform.localRotation = Quaternion.Euler(xRotation, yRotation, zRotation += rotationSpeed);
+            
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            transform.localRotation = Quaternion.Euler(xRotation, yRotation, zRotation -= rotationSpeed);
+            
+        }
+        
+    }
+}
---
+++ Assets/Scenes/MovementInput.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementInput : MonoBehaviour
+{
+    public CharacterController charController;
+    public float speed = 10f;
+    Vector3 Velocity;
+    public Transform groundcheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    public float gravity = -9f;
+    bool onGround;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        onGround = Physics.CheckSphere(groundcheck.position, groundDistance, groundMask);
+
+        if(onGround && Velocity.y <= 0)
+        {
+            Velocity.y = -2f;
+        }
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        charController.Move(move * speed);
+
+        Velocity.y += gravity * Time.deltaTime;
+        charController.Move(Velocity * Time.deltaTime);
+        
+        if(Input.GetButton("Jump") && onGround)
+        {
+            Velocity.y = Mathf.Sqrt(1f * -2f * gravity);
+            AudioSource jump = GetComponent<AudioSource>();
+            jump.Play();
+        }
+    }
+}
